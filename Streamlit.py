d5ee9b303ecea34fb7545a63412280c8978f29d8
samukelisiewenkosi <167233000+samukelisiewenkosi@users.noreply.github.com>
2024-07-23 20:07:11 +0200

    .

--- Streamlit.py
+++ Streamlit.py
@@ -1,24 +1,21 @@
 import streamlit as st
 
 # Title of the app
-st.title("My First Streamlit App")
+st.title("My Streamlit App")
 
-# Display text
-st.write("Hello, Streamlit!")
+# Add a header
+st.header("Welcome to my Streamlit app")
 
-# Display a dataframe
-import pandas as pd
-df = pd.DataFrame({
-    'Column 1': [1, 2, 3, 4],
-    'Column 2': [10, 20, 30, 40]
-})
-st.write("Here's a sample dataframe:")
-st.write(df)
+# Add a subheader
+st.subheader("This is a basic skeleton app")
+
+# Add some text
+st.write("This app doesn't use any vectorizers. It's a simple skeleton for you to build upon.")
 
 # Add a slider
-slider_value = st.slider("Select a value", 0, 100, 50)
-st.write("Slider value is:", slider_value)
+slider_value = st.slider("Select a value", 0, 100)
+st.write("Selected value:", slider_value)
 
 # Add a button
-if st.button("Click me!"):
+if st.button("Click me"):
     st.write("Button clicked!")
