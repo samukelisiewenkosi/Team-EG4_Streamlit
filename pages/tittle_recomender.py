4c5b7de06018dee1216519861e48129c8c2398e3
samukelisiewenkosi <167233000+samukelisiewenkosi@users.noreply.github.com>
2024-08-08 15:21:25 +0200

    Update tittle_recomender.py

--- pages/tittle_recomender.py
+++ pages/tittle_recomender.py
@@ -3,6 +3,8 @@ import pandas as pd
 import re
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+import os
+import pickle
 
 # Load the dataset
 def load_data():
